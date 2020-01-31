3415300ede4a68060bdef0f74c1f29177eaa83c6
Rongxin Liu <rongxinliu.dev@gmail.com>
2020-01-30 19:18:31 -0500

    minor tweak on /src/logger.js

--- src/logger.js
+++ src/logger.js
@@ -3,9 +3,4 @@ const { logger } = window
 logger.transports.file.appName = 'publish50'
 logger.transports.file.fileName = 'renderer.log'
 
-// TODO
-function error (arg) {
-  return
-}
-
-export { logger, error }
+module.exports = logger
